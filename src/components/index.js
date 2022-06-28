411ad8070c2c1cd53ea78473e8ecb9b35b651a10
ismailonur <ismailonur15@gmail.com>
2022-06-28 12:59:13 +0300

    Created folder and file

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { About } from './about/About.jsx';
+export { Contact } from './contact/Contact.jsx';
+export { Experience } from './experience/Experience.jsx';
+export { Footer } from './footer/Footer.jsx';
+export { Header } from './header/Header.jsx';
+export { Nav } from './nav/Nav.jsx';
+export { Portfolio } from './portfolio/Portfolio.jsx';
+export { Services } from './services/Services.jsx';
+export { Testimonials } from './testimonials/Testimonials.jsx';
