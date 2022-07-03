a8a5967536972ab869f34e9525f2e86ae03e4a70
ismailonur <ismailonur15@gmail.com>
2022-07-03 17:31:49 +0300

    header jsx and css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './header.css'
 
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>İsmail Onur</h1>
+        <h5>Software Engineer</h5>
+
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt='me' />
+        </div>
+
+        <a href="#contacts" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
