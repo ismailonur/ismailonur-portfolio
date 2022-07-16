e42dd1db7eaec1179f16295e6e3c4084942c3326
ismailonur <ismailonur15@gmail.com>
2022-07-16 15:35:41 +0300

    Footer Completed!

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './footer.css'
 
+import { FiInstagram } from 'react-icons/fi'
+import { IoLogoTwitter } from 'react-icons/io'
+
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer>
+      <a href='#' className='footer__logo'>İsmail Onur</a>
+
+      <ul className="permalinks">
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#experience'>Experience</a>
+        </li>
+        <li>
+          <a href='#services'>Services</a>
+        </li>
+        <li>
+          <a href='#portfolio'>Portfolio</a>
+        </li>
+        <li>
+          <a href='#testimonials'>Testimonials</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href='https://instagram.com'><FiInstagram /></a>
+        <a href='https://twitter.com'><IoLogoTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>
+          &copy; Copyright 2022. All rights reserved.
+        </small>
+      </div>
+    </footer>
   )
 }
 
-export {Footer}
+export { Footer }
