094ec78f565daa57826a7a2fbedc2e8487baf17f
ismailonur <ismailonur15@gmail.com>
2022-08-14 18:39:27 +0300

    Edited About

--- src/screens/about/About.jsx
+++ src/screens/about/About.jsx
@@ -4,8 +4,16 @@ import ME from '../../assets/me-about.jpg'
 import { FaAward } from 'react-icons/fa'
 import { FiUsers } from 'react-icons/fi'
 import { VscFolderLibrary } from 'react-icons/vsc'
+import { useEffect } from 'react'
 
 const About = () => {
+  const [experienceYears, setExperienceYears] = React.useState(0)
+
+  useEffect(() => {
+    const temp = new Date().getFullYear();
+    setExperienceYears(temp - 2019)
+  }, [])
+
   return (
     <section id='about'>
       <h5>Get To Know</h5>
@@ -23,23 +31,27 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon' />
               <h5>Experience</h5>
-              <small>3+ Years Working</small>
+              <small>{experienceYears}+ Years Working</small>
             </article>
 
             <article className='about__card'>
               <FiUsers className='about__icon' />
-              <h5>Clients</h5>
-              <small>200+ Wordwide</small>
+              <h5>temp</h5>
+              <small>temp</small>
             </article>
 
             <article className='about__card'>
               <VscFolderLibrary className='about__icon' />
               <h5>Projects</h5>
-              <small>10+ Complated</small>
+              <small>15+ Complated</small>
             </article>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolorem neque animi expedita possimus odio pariatur ullam totam obcaecati, sequi dolore et eius iste exercitationem beatae non adipisci quae doloribus.
+            I am a software engineer who is passionate about building mobile and web applications.
+            I have a strong background in mobile development and have worked on multiple projects.
+            I have been developing software in the field of React-Native for {experienceYears} years.
+            I also do web development using react.js.
+            I worked as a Unity game developer for 6 months.
           </p>
           <a href='#contact' className='btn btn-primary'>Let's Talk</a>
         </div>
