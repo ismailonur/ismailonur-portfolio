815d648dc4092708234669711b48df5cdee678a7
ismailonur <ismailonur15@gmail.com>
2022-08-17 11:07:30 +0300

    eth gwei tracker pravicy policy edit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
         <>
             <Routes>
                 <Route path="/" element={<Main />} />
-                <Route path="pol" element={<Policy />} />
+                <Route path="ethereum_gwei_tracker_privacy_policy" element={<Policy />} />
             </Routes>
         </>
     )
