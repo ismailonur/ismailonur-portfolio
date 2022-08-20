9857ab4f8e0f341d0c4f31174570b521942df3ec
ismailonur <ismailonur15@gmail.com>
2022-08-20 22:25:26 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import {
 
 import ReactGA from 'react-ga';
 const TRACKING_ID = "UA-150454474-1";
-ReactGA.initialize(TRACKING_ID);
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
-    console.log('====================', TRACKING_ID, 1)
+    console.log('====================', TRACKING_ID)
+    useEffect(() => {
+        ReactGA.initialize(TRACKING_ID);
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
 
     return (
         <>
