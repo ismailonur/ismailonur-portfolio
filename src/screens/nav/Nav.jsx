83fc6e6b696d8e3ad788016de918fec83c63950e
ismailonur <ismailonur15@gmail.com>
2022-08-20 22:50:47 +0300

    Add useAnalyticsEventTracker

--- src/screens/nav/Nav.jsx
+++ src/screens/nav/Nav.jsx
@@ -8,38 +8,47 @@ import { BiBook } from 'react-icons/bi'
 import { RiServiceLine } from 'react-icons/ri'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 
+import useAnalyticsEventTracker from '../../components/useAnalyticsEventTracker/useAnalyticsEventTracker'
+
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
+
+  const gaEventTracker = useAnalyticsEventTracker();
+
+  const handleNavClick = (text) => {
+    setActiveNav(text)
+    gaEventTracker(text)
+  }
 
   return (
     <nav>
       <a
         href="#"
-        onClick={() => setActiveNav('#')}
+        onClick={() => handleNavClick('#')}
         className={activeNav === '#' ? 'active' : ''}>
         <AiOutlineHome />
       </a>
       <a
         href="#about"
-        onClick={() => setActiveNav('#about')}
+        onClick={() => handleNavClick('#about')}
         className={activeNav === '#about' ? 'active' : ''}>
         <AiOutlineUser />
       </a>
       <a
         href="#blog"
-        onClick={() => setActiveNav('#blog')}
+        onClick={() => handleNavClick('#blog')}
         className={activeNav === '#experience' ? 'active' : ''}>
         <BiBook />
       </a>
       <a
         href="#portfolio"
-        onClick={() => setActiveNav('#portfolio')}
+        onClick={() => handleNavClick('#portfolio')}
         className={activeNav === '#services' ? 'active' : ''}>
         <RiServiceLine />
       </a>
       <a
         href="#contact"
-        onClick={() => setActiveNav('#contact')}
+        onClick={() => handleNavClick('#contact')}
         className={activeNav === '#contact' ? 'active' : ''}>
         <BiMessageSquareDetail />
       </a>
